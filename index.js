123d6b63cec2759250a6ae27dde7b0ffbfeeaf00
Pedro Almeida <p.almeida@live.com>
2022-07-27 12:36:05 -0400

    .

--- index.js
+++ index.js
@@ -2,16 +2,40 @@
 $('h1').append(" de pessoas"); // Add conteúdo no FINAL da tag, class, id
 $('.meu_nome').html("Pedrinho"); // Troca, coloca conteúdo dentro da tag, class, id
 
-$('#button').on("click", botao); // lê o click do botão
+
 $('#button').hover(function () {
         // over
-        $('.meu_nome').css({'color':'blue', 'font-family': 'roboto',
+        $('.meu_nome').css({'color':'blue', 'font-family': 'arial'
+    })},
+        function(){
+        $('.meu_nome').css({
+        'color':'red',
+        'font-family': 'arial',
     });
 });
 
-$('document').ready(function(){ // Assim que o documento estiver pronto executará um função
-    $('h1').attr("variavel");
+$('document').ready(function sitePronto(){ // Assim que o documento estiver pronto executará um função
+   
+    $('#button').on("click", botao); // lê o click do botão
+    
+    $('li').on('click', lista);
+    $('.input').focus();
+    fundoVermeio();
+
+    
 });
+
+function fundoVermeio(){ 
+    $(this).css({
+        'background-color':'red'
+    });
+}
+
+function lista() {
+    $(this).html("Muda quando"); //This muda apenas o item clicado
+    $(this).append("|clica|")
+
+};
 
 function botao(){
 
@@ -20,7 +44,9 @@ function botao(){
     $('li').css({
         'color': 'hotpink',
         'text-align': 'center',
-        'font-family': 'roboto',
+        'font-family': 'arial',
         'list-style-type': 'none'
     });
 };
+
+
